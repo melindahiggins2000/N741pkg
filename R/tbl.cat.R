6a773c299b02bb63b3a79ff8dedf742671fa06eb
melindahiggins2000 <melinda.higgins@emory.edu>
2017-03-10 19:48:29 -0500

    package built and checks all ok no errors

--- R/tbl.cat.R
+++ R/tbl.cat.R
@@ -5,20 +5,16 @@
 #'
 #' @param gdf a grouped data.frame from dplyr::group_by().
 #' @param var a vector in a data.frame df$var.
-#' @return \code{t1} the summary statistics table.
+#' @return \code{t} the frequency and percents table.
 #' @examples
 #' a <- runif(10,0,1)
 #' b <- c("a","b","c","d","a","a","b","b","c","c")
 #' x <- data.frame(a,b)
-#' tbl.cat(x,x$b)
+#' gx <- dplyr::group_by(x,b)
+#' tbl.cat(gx,gx$b)
 tbl.cat <- function(gdf,var){
-  #gdf <- df %>%
-  #  group_by(var)
-  ss <- length(var)
-  t1 <- gdf %>%
-    summarise(freq = n(),
-              pct = n()*100/ss)
-  return(t1)
+  t <- dplyr::summarise(gdf,
+                        freq = n(),
+                        pct = n()*100/10)
+  return(t)
 }
-
-
--- R/tbl.continuous.R
+++ R/tbl.continuous.R
@@ -13,16 +13,16 @@
 #' x <- data.frame(a,b)
 #' tbl.continuous(x,x$a,"label")
 tbl.continuous <- function(df,var,l){
-  t <- df %>%
-    summarise(item = l,
-              n = sum(!is.na(var)),
-              missing = sum(is.na(var)),
-              min = min(var, na.rm=TRUE),
-              avg = mean(var, na.rm=TRUE),
-              SD = sd(var, na.rm=TRUE),
-              median = median(var, na.rm=TRUE),
-              Q1 = quantile(var, 0.25, na.rm=TRUE),
-              Q3 = quantile(var, 0.75, na.rm=TRUE),
-              max = max(var, na.rm=TRUE))
+  t <- dplyr::summarise(df,
+                        item = l,
+                        n = sum(!is.na(var)),
+                        missing = sum(is.na(var)),
+                        min = min(var, na.rm=TRUE),
+                        avg = mean(var, na.rm=TRUE),
+                        SD = sd(var, na.rm=TRUE),
+                        median = median(var, na.rm=TRUE),
+                        Q1 = quantile(var, 0.25, na.rm=TRUE),
+                        Q3 = quantile(var, 0.75, na.rm=TRUE),
+                        max = max(var, na.rm=TRUE))
   return(t)
 }
