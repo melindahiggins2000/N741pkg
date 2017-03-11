296a4ecf81dbd9af4befa4e9203b784a5f345cd1
melindahiggins2000 <melinda.higgins@emory.edu>
2017-03-11 06:59:19 -0500

    update pkg documentation

--- R/N741pkg.R
+++ R/N741pkg.R
@@ -1,12 +1,25 @@
 #' N741pkg: A package for N741 course.
 #'
 #' N741pkg provides a few useful functions and a
-#' template for reports and analyses in N741 course.
+#' template for reports and analyses in the N741
+#' Big Data Analytics for Healthcare course
+#' \url{https://melindahiggins2000.github.io/N741bigdata/}.
 #'
-#' It has three main goals:
+#' @section N741pkg functions:
+#' The functions included are:
 #'
 #' \itemize{
-#' \item goal 1.
+#' \item tbl.cat: creates a table of frequencies and percentages
+#' for a categorical variable
+#' \item tbl.continuous: creates a table of summary statistics
+#' for a numeric variable
+#' }
+#'
+#' @section Goals:
+#' The N741pkg package has the following goals:
+#'
+#' \itemize{
+#' \item to provide a Rmarkdown template.
 #' \item goal 2.
 #' \item goal 3.
 #' }
