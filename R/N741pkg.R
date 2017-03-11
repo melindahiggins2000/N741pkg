b808609fa5020e8b2da87efe9ce857b72f0c865a
melindahiggins2000 <melinda.higgins@emory.edu>
2017-03-10 21:58:41 -0500

    add package description

---
+++ R/N741pkg.R
@@ -0,0 +1,19 @@
+#' N741pkg: package for N741 course
+#'
+#' N741pkg provides a few useful functions and a
+#' template for reports and analyses in N741 course.
+#'
+#' It has three main goals:
+#'
+#' \itemize{
+#' \item goal 1.
+#' \item goal 2.
+#' \item goal 3.
+#' }
+#'
+#' To learn more about N741pkg, start with the vignettes:
+#' `browseVignettes(package = "N741pkg")`
+#'
+#' @docType package
+#' @name N741pkg
+NULL
