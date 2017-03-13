a74dc3101ab93aef56bc3dced4ca49a22fb187e9
melindahiggins2000 <melinda.higgins@emory.edu>
2017-03-13 12:05:59 -0400

    update vignette build

--- vignettes/vignette1.R
+++ vignettes/vignette1.R
@@ -1,27 +1,31 @@
 ## ------------------------------------------------------------------------
-library(N741pkg)
-a <- runif(10,0,1)
-b <- c("a","b","c","d","a","a","b","b","c","c")
-x <- data.frame(a,b)
-c <- c("blue","blue","blue","blue","red",
-       "green","green","green","black","orange")
-d <- c(2,40,3,NA,5,1,2,4,3,NA)
-x <- data.frame(a,b,c,d)
-
-knitr::kable(x)
-
-t <- N741pkg::tbl.continuous(x,x$d,"label ddd")
-knitr::kable(t)
+m <- mtcars
+t1 <- N741pkg::tbl.continuous(m,m$mpg,"Miles per Gallon")
+t2 <- N741pkg::tbl.continuous(m,m$disp,"Engine Displacement")
+t3 <- N741pkg::tbl.continuous(m,m$wt,"Car Weight")
+t4 <- N741pkg::tbl.continuous(m,m$hp,"Horsepower")
+t5 <- N741pkg::tbl.continuous(m,m$qsec,"1/4 mile time")
+knitr::kable(rbind(t1,t2,t3,t4,t5),
+             caption = "Table of Summary Stats for Numeric Variables in mtcars")
 
 ## ------------------------------------------------------------------------
-gx <- dplyr::group_by(x,c)
-t <- N741pkg::tbl.cat(gx,gx$c)
-knitr::kable(t)
+# create grouped data.frame of m by cylinders
+gm <- dplyr::group_by(m,cyl)
+t1 <- N741pkg::tbl.cat(gm,gm$cyl)
+knitr::kable(t1,
+             caption = "Frequency Table for Number of Cylinders")
 
-gx <- dplyr::group_by(x,as.factor(d))
-t <- N741pkg::tbl.cat(gx,gx$d)
-knitr::kable(t)
+# create grouped data.frame of m by am
+gm <- dplyr::group_by(m,am)
+t1 <- N741pkg::tbl.cat(gm,gm$am)
+knitr::kable(t1,
+             caption = "Frequency Table for Transmission")
 
+# create grouped data.frame of m by gear
+gm <- dplyr::group_by(m,gear)
+t1 <- N741pkg::tbl.cat(gm,gm$gear)
+knitr::kable(t1,
+             caption = "Frequency Table for Number of Forward Gears")
 
 ## ---- fig.show='hold'----------------------------------------------------
 plot(1:10)
